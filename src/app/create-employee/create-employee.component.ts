63bbe67dfdcf1d3ff493a5651a21975f5200b851
[VivekGarg1] <[vivekgarg020@gmail.com]>
2020-09-02 17:30:00 +0530

    code mereg

---
+++ src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../service/employee.service';
+import { Router } from '@angular/router';
+import { Employee } from '../model/Employee';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  employee: Employee = new Employee();
+  submitted = false;
+  constructor(private employeeService: EmployeeService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  save() {
+    this.employeeService.createEmployee(this.employee).subscribe( data => {
+      console.log(data);
+    },
+      error => console.log(error));
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
+
+  gotoList() {
+    this.router.navigate(['/employees']);
+  }
+
+}
